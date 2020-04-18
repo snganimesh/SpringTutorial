75d72d17964c7d3ca9fd5a2516a60c73e7638f6f
snganimesh <sng.animesh@gmail.com>
2020-04-19 03:24:10 +0530

    Changes to Create Table

--- src/main/java/com/example/HomeResource.java
+++ src/main/java/com/example/HomeResource.java
@@ -1,11 +1,20 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HomeResource {
 
+
+          @Autowired
+           MyDetailsService myDetailsService;
+          @Autowired
+          UserRepository userRepository;
          @GetMapping("/")
           public String home()
          {
@@ -16,11 +25,24 @@ public class HomeResource {
          public String user()
          {
              return ("<h1>Welcome User</h1>");
-             }
+         }
         @GetMapping("/admin")
         public String admin()
         {
             return ("<h1>Welcome Admin</h1>");
+        }
+
+        @GetMapping("/create")
+        public List<User> createUser()
+        {
+            User user = new User();
+            user.setUserName("Ani");
+            user.setPassword("Pass");
+            user.setActive(true);
+            user.setRoles("ROLE_USER");
+            userRepository.save(user);
+
+            return userRepository.findAll();
         }
 
 }
